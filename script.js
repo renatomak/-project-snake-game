1c69ffde8d0faaeae268c0acda780d2c22f6ca83
renatomak <renato.mark.silva@gmail.com>
2021-03-13 15:59:58 -0300

    incremento dos movimentos da cobrinha

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Canvas_API
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -21,7 +22,21 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+  if(event.keyCode == 37 && direction !== "right") direction = "left"; 
+  if(event.keyCode == 38 && direction !== "down") direction = "up"; 
+  if(event.keyCode == 39 && direction !== "left") direction = "right"; 
+  if(event.keyCode == 40 && direction !== "up") direction = "down"; 
+}
+
 function iniciarJogo() {
+  if(snake[0].x > 15 * box && direction === "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction === "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction === "drow") snake[0].y = 0;
+  if(snake[0].y < 0 && direction === "up") snake[0].y = 16 * box;
+
   criarBR();
   criarCobrinha();
 
