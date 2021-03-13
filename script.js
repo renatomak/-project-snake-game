8e83cf62dae1452a11816f92108f9da874c1f6e5
renatomak <renato.mark.silva@gmail.com>
2021-03-13 15:36:10 -0300

    criaçao dos primeiros movimentos

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
-}
+};
 
 let direction = "right";
 
@@ -17,9 +17,30 @@ function criarBR() {
 function criarCobrinha() {
   for (let index = 0; index < snake.length; index++) {
     context.fillStyle = "green";
-    context.fillRect(snake[index].x, snake[index].y, box, box);    
+    context.fillRect(snake[index].x, snake[index].y, box, box);
   }
 }
 
-criarBR();
-criarCobrinha();
+function iniciarJogo() {
+  criarBR();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
